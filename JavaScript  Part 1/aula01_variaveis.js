099d3c770c29dfc3a2f1cfda773b7eb2be4608d5
domdq <domborgesbr2468@gmail.com>
2025-01-07 11:38:27 -0300

    Primeiro commit

---
+++ JavaScript  Part 1/aula01_variaveis.js
@@ -0,0 +1,15 @@
+// Variáveis
+// voce pode guarda um valor e uma variáveis e um nome único que não pode mudar sendo strings,boolean e numero
+
+var boolean = true; // ou false
+let number = 15;
+const string = "algo";
+
+// Permitido
+// iniciar com $ ou _
+// colocar acentos
+// iniciar com leras tanto maiúscula e minuscula
+
+// não pode
+// iniciar com numero
+// colocar espaço
