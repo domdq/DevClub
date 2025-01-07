8a1efedb285f8870ed98e6e9f4355f52de80424a
domdq <domborgesbr2468@gmail.com>
2025-01-07 11:59:13 -0300

    aula02

---
+++ JavaScript  Part 1/aula02_Object.js
@@ -0,0 +1,15 @@
+/* Object 
+
+propriedade : valor
+
+*/
+
+const myObject = {
+  nome: "Dom",
+  idd: 23,
+  altura: 1.67,
+  sex: "M",
+};
+
+console.log(myObject);
+console.log(myObject.nome); // vai mostra o valor da propriedade nome
