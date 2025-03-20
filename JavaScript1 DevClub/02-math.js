e4813f66b109b5bf8aa6a6f35b5ad1b1932f574e
domdq <domborgesbr2468@gmail.com>
2025-03-20 20:55:12 -0300

    JavaScript1 DevClub

---
+++ JavaScript1 DevClub/02-math.js
@@ -0,0 +1,7 @@
+// MATH
+Math.PI(); // mostra o valor do PI
+Math.pow(5, 6); // Usarmos Potenciação
+Math.sqrt(25, 3); // permite calcular a raiz de um numero
+math.random(); // mostrar um numero aleatório entre 0 a 1
+math.ceil(23.5); // arredondar para cima
+math.floor(23.5); //  arredondar para baixo
---
+++ JavaScript1 DevClub/desafio1.js
@@ -0,0 +1,16 @@
+// Desafio 1
+let num1 = 5;
+let num2 = 9;
+let soma = num1 + num2;
+console.log(soma);
+
+// Desafio 2
+soma = num1 * num2;
+let multiplicar = soma + 10;
+console.log(multiplicar);
+
+// Desafio 3
+let raiz = Math.sqrt(25);
+multiplicar = raiz * 10;
+let divisao = multiplicar / 33;
+console.log(divisao);
