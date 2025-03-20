593ea6d74f02a1955958673e8e7f4ed379361091
domdq <domborgesbr2468@gmail.com>
2025-03-19 21:48:44 -0300

    DevClub JavaScript 01-variaveis

---
+++ JavaScript DevClub/01-variaveis.js
@@ -0,0 +1,14 @@
+// Variáveis
+var num1 = 1; // não user o var
+let num2 = 2;
+const num3 = 3; // const não pode mudar o valor
+
+// Permitido:
+// - Iniciar com $ ou _
+// - colocar acentos
+// - Iniciar com letras
+// - letras maiúsculas e minusculas
+
+// Não Pode:
+// - iniciar com números
+// - colocar espaçoS
